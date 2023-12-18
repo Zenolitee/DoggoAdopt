07d73f2280fd483387b5304d5ac8a8f0385f2f35
Zense <73891112+Zenolitee@users.noreply.github.com>
2023-12-18 21:15:50 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,51 @@
 const express = require('express')
 const app = express()
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database(':memory:')
+const cors = require('cors')
 
 
+
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+app.use(express.json({limit: '10mb'}))
+
+let db = new sqlite3.Database('dogadoptdb.db', (err) => {
+    if(err){
+        console.error(err.message)
+    }console.log('Connected to the database...')
+})
+
+
+app.post('/validatePassword', (req, res) => {
+    const {username, password } = req.body
+
+    db.all(`select * from credentials where username = '${username}' and password = '${password}'`, (err, rows) => {
+        if(err){
+            throw err;
+        }
+        if(rows.length > 0){
+            res.send({validation:true})
+        }else{
+            res.send({validation:false})
+        }
+    })
+
+    
+})
+
+app.listen(5000, () => {console.log("Server is alive nigga.")})
+
+
+
+/*
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"] })
 })
 
-app.listen(5000, () => {console.log("Server is alive.")})
+
 
 db.serialize(() => {
     db.run('CREATE TABLE lorem (info TEXT)')
@@ -25,4 +62,4 @@ db.serialize(() => {
     })
   })
   
-  db.close()
+  db.close()*/
