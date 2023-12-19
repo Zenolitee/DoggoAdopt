059152fade8f2d27f1b66000c208134c7c514dc8
Zense <73891112+Zenolitee@users.noreply.github.com>
2023-12-20 00:29:41 +0800

    [fixed port]

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 // src/App.js
 import AppRouter from './components/AppRouter';
-import React, { useEffect, useState } from 'react'
+
 
 function App(){
 
 
-const [backendData, setBackendData] = useState([{}])
 
-useEffect(()=> {
-  fetch("/api").then(
-    response=>response.json()
-  ).then(
-    data =>{
-      setBackendData(data)
-    }
-  )
-})
 
   return (
     <div className="App ">
