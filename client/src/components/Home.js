2949b34b9df1fd029181fb556fd6c79aba3ac213
Zense <73891112+Zenolitee@users.noreply.github.com>
2023-12-20 00:12:29 +0800

    [nothing here]

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -19,8 +19,6 @@ const Home = () => {
     <button type="button" class="focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-1 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ml-[4rem] mt-[1rem]">Learn More</button>
     
   </div>
-    
-  
 </div>
   
 
