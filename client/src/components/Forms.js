8bba5cb3de7de3534500512f63a0eb6eb1c49226
cheWy20k <enricoabsin02@gmail.com>
2023-12-23 02:03:25 +0800

    added form fileds

--- client/src/components/Forms.js
+++ client/src/components/Forms.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import "../css/Forms.css";
+import Navbar from './Navbar.js';
 
 const Forms = () => {
   const { PetName } = useParams();
   const [petDetails, setPetDetails] = useState(null);
+  const [adoptFormData, setAdoptFormData] = useState({
+    fullName: '',
+    contactNumber: '',
+    reasonForAdopting: '',
+    validID: '',
+  });
 
   useEffect(() => {
     // Fetch pet details based on the PetName parameter
@@ -25,22 +33,96 @@ const Forms = () => {
       });
   }, [PetName]);
 
-  return (
-    <div>
-      <h1>Adopting {PetName}</h1>
-      
-      {petDetails ? (
-        <div>
-          <p>Birthday: {petDetails.DateOfBirth}</p>
-          <p>Owner: {petDetails.OwnerName}</p>
-          <p>Description: {petDetails.Description}</p>
-          {/* Add more details as needed */}
-        </div>
-      ) : (
-        <p>Loading pet details...</p>
-      )}
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAdoptFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-      {/* Other form components */}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to submit the adoption form data
+    console.log('Adoption form submitted:', adoptFormData);
+    // You can send this data to the server or perform further actions
+  };
+
+  return (
+    <div className="container flex max-w-none">
+      <Navbar />
+      <div className="background-image flex items-center justify-center ml-12">
+        <div className="rectangle-shape bg-gray-300 rounded-lg w-[40rem] h-[40rem] bg-opacity-20 mt-[5rem] ml-[5rem] flex flex-col">
+          <h1>Adopting {PetName}</h1>
+        
+          {petDetails ? (
+            <div>
+              <p>Birthday: {petDetails.DateOfBirth}</p>
+              <p>Owner: {petDetails.OwnerName}</p>
+              <p>Description: {petDetails.Description}</p>
+              {/* Add more details as needed */}
+            </div>
+          ) : (
+            <p>Loading pet details...</p>
+          )}
+
+          <form onSubmit={handleSubmit}>
+            <div className="text-green-400 font-bold">
+              <label>
+                Full Name:
+                <input
+                  type="text"
+                  name="fullName"
+                  value={adoptFormData.fullName}
+                  onChange={handleInputChange}
+                  className="mt-1 p-2 w-[25rem] border rounded-md"
+                />
+              </label>
+            </div>
+
+            <div className="text-green-400 font-bold">
+              <label>
+                Contact Number:
+                <input
+                  type="text"
+                  name="contactNumber"
+                  value={adoptFormData.contactNumber}
+                  onChange={handleInputChange}
+                  className="mt-1 p-2 w-[25rem] border rounded-md"
+                />
+              </label>
+            </div>
+
+            <div className="text-green-400 font-bold">
+              <label>
+                Reason for Adopting:
+                <textarea
+                  name="reasonForAdopting"
+                  value={adoptFormData.reasonForAdopting}
+                  onChange={handleInputChange}
+                  className="text-black-500 mt-1 p-2 w-[25rem] border rounded-md"
+                />
+              </label>
+            </div>
+
+            <div className="text-green-400 font-bold">
+              <label>
+                Valid ID:
+                <input
+                  type="text"
+                  name="validID"
+                  value={adoptFormData.validID}
+                  onChange={handleInputChange}
+                  className="mt-1 p-2 w-[25rem] border rounded-md text-black-500"
+                />
+              </label>
+            </div>
+
+            <button type="submit" className="bg-blue-500
+                text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300 ml-[1rem]">Submit Adoption Form</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
